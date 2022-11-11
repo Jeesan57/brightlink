141bce58f4da191c6ef16efab99286d29a942d20
Dark_Shadow <dark@Dark.Dark.com>
2022-11-11 11:35:19 +0600

    update tag part

--- js/quotes.js
+++ js/quotes.js
@@ -1,41 +1,41 @@
 
 
-let changeTime = 3;
-// key frame for background and text animation
-function addKeyFrames() {
-    const keyFrames = document.createElement("style");
-    keyFrames.innerHTML = `
+// let changeTime = 3;
+// // key frame for background and text animation
+// function addKeyFrames() {
+//     const keyFrames = document.createElement("style");
+//     keyFrames.innerHTML = `
  
-  @keyframes textAnimation {
-    0%{
-      content: "MOVING QUOTES";
-    }
+//   @keyframes textAnimation {
+//     0%{
+//       content: "MOVING QUOTES";
+//     }
   
-    50%{
-      opacity: 0;
-    }
+//     50%{
+//       opacity: 0;
+//     }
 
 
-    100%{
-      content: "ONLINE RELOCATION QUOTES";
+//     100%{
+//       content: "ONLINE RELOCATION QUOTES";
 
-    }
-  }
+//     }
+//   }
   
-    #banner-primary-text:after {
-      animation: textAnimation ${changeTime}s linear infinite alternate;
-      content: "MOVING QUOTES";
-      display: block;
-      z-index: 15;
-    }
+//     #banner-primary-text:after {
+//       animation: textAnimation ${changeTime}s linear infinite alternate;
+//       content: "MOVING QUOTES";
+//       display: block;
+//       z-index: 15;
+//     }
     
-  `;
-    document.head.appendChild(keyFrames);
-}
+//   `;
+//     document.head.appendChild(keyFrames);
+// }
 
 
-function startInterval() {
-    addKeyFrames();
-}
+// function startInterval() {
+//     addKeyFrames();
+// }
 
-startInterval();
+// startInterval();
